13305efb3968dc567568352daca6282e85f8f11f
Kviktor10 <117000550+Kviktor10@users.noreply.github.com>
2026-03-17 16:07:39 -0300

    Update arquivos.js

--- assets/js/arquivos.js
+++ assets/js/arquivos.js
@@ -3,57 +3,49 @@ const bancoDeImagens = [
         id: 1,
         nome: "Capoeira em Foco",
         formato: "1:1",
-        preview: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/MESTRE_JUNIOR.png",
-        download: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/MESTRE_JUNIOR.png"
+        url: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/MESTRE_JUNIOR.png"
     },
     {
         id: 2,
         nome: "Barriguda Games",
         formato: "1:1",
-        preview: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POSTAGEM_KVIKTOR.png",
-        download: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POSTAGEM_KVIKTOR.png"
+        url: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POSTAGEM_KVIKTOR.png"
     },
     {
         id: 3,
         nome: "Papo Contemporâneo",
         formato: "1:1",
-        preview: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_ELIMARCOS.png",
-        download: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_ELIMARCOS.png"
+        url: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_ELIMARCOS.png"
     },
     {
         id: 4,
         nome: "PodTeen",
         formato: "1:1",
-        preview: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_GLENDA_SOUZA.png",
-        download: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_GLENDA_SOUZA.png"
+        url: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_GLENDA_SOUZA.png"
     },
     {
         id: 5,
         nome: "Helicoptéro Musical",
         formato: "1:1",
-        preview: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_HELICOPTERO_MUSICAL.png",
-        download: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_HELICOPTERO_MUSICAL.png"
+        url: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_HELICOPTERO_MUSICAL.png"
     },
     {
         id: 6,
         nome: "O poder da Oração",
         formato: "1:1",
-        preview: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_MARIO_DOURADO.png",
-        download: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_MARIO_DOURADO.png"
+        url:"https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_MARIO_DOURADO.png"
     },
         {
         id: 7,
         nome: "GAIOLA VÁZIA",
         formato: "1:1",
-        preview: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_PERICLES_BARRETO.png",
-        download: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_PERICLES_BARRETO.png"
+        url: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_PERICLES_BARRETO.png"
     },
             {
         id: 8,
         nome: "HUMOR, CORDEL E PARÓDIA",
         formato: "1:1",
-        preview: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_QUADRADO_GILSON_BARBOSA.png",
-        download: "https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_QUADRADO_GILSON_BARBOSA.png"
+        url:"https://raw.githubusercontent.com/Kviktor10/barrigudawebtv/main/fotos/POST_QUADRADO_GILSON_BARBOSA.png"
     }
 ];
 
